93e43382aef2f8b4925b7bb8548aa0e06a417041
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-01-09 21:19:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { Link, NavLink } from 'react-router-dom'
+import Header from "./components/Header.jsx";
+import Footer from "./components/footer.jsx";
+import Sidebar from "./components/sidebar.jsx";
 
 function App() {
 
   return (
-    
+      <Header />
+      <Sidebar />
+      <Footer />
   )
 }
 
