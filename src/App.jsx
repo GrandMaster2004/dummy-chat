a09574ab38b25e505863fec8c49e2799e7b3224d
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-01-09 20:48:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Link, NavLink } from 'react-router-dom'
+
+function App() {
+
+  return (
+    
+  )
+}
+
+export default App
---
+++ src/components/CreateBox.jsx
@@ -0,0 +1,16 @@
+
+function CreateBox() {
+    return (
+        <>
+        <div className="w-auto h-px bg-gray-300 m-3 text-zinc-800"/><div/>
+        <div className="mt-2">
+            <button className="flex items-center space-x-3">
+                <span className="p-1 flex items-center justify-center w-6 h-6 bg-rose-400 rounded-full text-black text-xl cursor-pointer shadow-md">+</span>
+                <span className="text-lg">Create new bot</span>
+            </button>
+        </div>
+        </>
+    )
+}
+
+export default CreateBox
